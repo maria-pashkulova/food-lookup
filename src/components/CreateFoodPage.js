25b97c65fc10414fbff51d41992cf635383445f0
MariaIP28 <mariapashkulova28@gmail.com>
2024-04-01 20:29:42 +0300

    UseNavigate hook

--- src/components/CreateFoodPage.js
+++ src/components/CreateFoodPage.js
@@ -3,7 +3,7 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 
-import { Link, Navigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import { createFoodItem } from '../api/foodService';
 
@@ -17,8 +17,8 @@ const formInitialState = {
 
 function CreateFoodPage() {
 
+    const navigate = useNavigate();
     const [foodData, setFoodData] = useState(formInitialState);
-    const [redirect, setRedirect] = useState(false);
 
     const handleInputChange = (e) => {
 
@@ -33,78 +33,75 @@ function CreateFoodPage() {
         e.preventDefault();
 
         createFoodItem(foodData)
-            .then(_ => setRedirect(true))
+            .then(() => navigate('/'))
             .catch(err => console.log(err));
     }
 
     return (
-        <>
-            {redirect && <Navigate to='/' />}
-            <div className='container mt-4 w-50'>
-                <h3>Create Food Item</h3>
-                <Form onSubmit={handleFormSubmit} className='mt-4'>
-                    <Row className="mb-3">
-                        <Form.Group as={Col} controlId="description">
-                            <Form.Label>Description</Form.Label>
-                            <Form.Control
-                                type="text"
-                                name="description"
-                                value={foodData.description}
-                                onChange={handleInputChange}
-                                placeholder="Enter description" />
-                        </Form.Group>
-                    </Row>
-                    <Row className="mb-3">
-                        <Form.Group as={Col} controlId="kcal">
-                            <Form.Label>Kcal</Form.Label>
-                            <Form.Control
-                                type="number"
-                                name="kcal"
-                                value={foodData.kcal}
-                                onChange={handleInputChange}
-                                placeholder="Enter kcal" />
-                        </Form.Group>
-                        <Form.Group as={Col} controlId="protein">
-                            <Form.Label>Protein</Form.Label>
-                            <Form.Control
-                                type="number"
-                                name="protein"
-                                value={foodData.protein}
-                                onChange={handleInputChange}
-                                placeholder="Enter grams of protein" />
-                        </Form.Group>
-                    </Row>
+        <div className='container mt-4 w-50'>
+            <h3>Create Food Item</h3>
+            <Form onSubmit={handleFormSubmit} className='mt-4'>
+                <Row className="mb-3">
+                    <Form.Group as={Col} controlId="description">
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control
+                            type="text"
+                            name="description"
+                            value={foodData.description}
+                            onChange={handleInputChange}
+                            placeholder="Enter description" />
+                    </Form.Group>
+                </Row>
+                <Row className="mb-3">
+                    <Form.Group as={Col} controlId="kcal">
+                        <Form.Label>Kcal</Form.Label>
+                        <Form.Control
+                            type="number"
+                            name="kcal"
+                            value={foodData.kcal}
+                            onChange={handleInputChange}
+                            placeholder="Enter kcal" />
+                    </Form.Group>
+                    <Form.Group as={Col} controlId="protein">
+                        <Form.Label>Protein</Form.Label>
+                        <Form.Control
+                            type="number"
+                            name="protein"
+                            value={foodData.protein}
+                            onChange={handleInputChange}
+                            placeholder="Enter grams of protein" />
+                    </Form.Group>
+                </Row>
 
-                    <Row className="mb-3">
-                        <Form.Group as={Col} controlId="fat">
-                            <Form.Label>Fat</Form.Label>
-                            <Form.Control
-                                type="number"
-                                name="fat"
-                                value={foodData.fat}
-                                onChange={handleInputChange}
-                                placeholder="Enter grams of fat" />
-                        </Form.Group>
-                        <Form.Group as={Col} controlId="carbs">
-                            <Form.Label>Carbs</Form.Label>
-                            <Form.Control
-                                type="number"
-                                name="carbs"
-                                value={foodData.carbs}
-                                onChange={handleInputChange}
-                                placeholder="Enter grams of carbs" />
-                        </Form.Group>
-                    </Row>
+                <Row className="mb-3">
+                    <Form.Group as={Col} controlId="fat">
+                        <Form.Label>Fat</Form.Label>
+                        <Form.Control
+                            type="number"
+                            name="fat"
+                            value={foodData.fat}
+                            onChange={handleInputChange}
+                            placeholder="Enter grams of fat" />
+                    </Form.Group>
+                    <Form.Group as={Col} controlId="carbs">
+                        <Form.Label>Carbs</Form.Label>
+                        <Form.Control
+                            type="number"
+                            name="carbs"
+                            value={foodData.carbs}
+                            onChange={handleInputChange}
+                            placeholder="Enter grams of carbs" />
+                    </Form.Group>
+                </Row>
 
-                    <Button className='me-2' variant="primary" type="submit">
-                        Create
-                    </Button>
-                    <Button as={Link} to="/" variant="primary" type="button">
-                        Back
-                    </Button>
-                </Form>
-            </div>
-        </>
+                <Button className='me-2' variant="primary" type="submit">
+                    Create
+                </Button>
+                <Button as={Link} to="/" variant="primary" type="button">
+                    Back
+                </Button>
+            </Form>
+        </div>
     )
 }
 
